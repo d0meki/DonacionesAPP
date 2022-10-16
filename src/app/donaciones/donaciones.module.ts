c809862e2586c558e046822697440440d7366660
Freddy Arriaga <domeki151190@gmail.com>
2022-10-15 22:50:22 -0400

    primer commit

---
+++ src/app/donaciones/donaciones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DonacionesRoutingModule } from './donaciones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DonacionesRoutingModule
+  ]
+})
+export class DonacionesModule { }
