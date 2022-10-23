f3de35b9438b0cf49da5cec530d4f2a2f140e7bb
IsaiasMCC <isaisvhsyw@gmail.com>
2022-10-23 11:54:11 -0400

    add to navbar and register fundacion

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 import { MenuComponent } from './menu/menu.component';
+import { Page404Component } from './page404/page404.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    Page404Component,
+    NavBarComponent,
+    FooterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
+  exports: [
+    NavBarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
