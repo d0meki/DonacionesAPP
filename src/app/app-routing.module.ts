b11875f0a60509bdbd04c173a6c2dec501ccf80d
unknown <domeki151190@gmail.com>
2022-12-23 17:10:35 -0400

    anim

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { RegisterComponent } from './auth/pages/register/register.component';
 import { PagosComponent } from './donaciones/pagos/pagos.component';
+import { PaypalComponent } from './donaciones/paypal/paypal.component';
 import { RegisterFundacionPageComponent } from './fundacion/pages/register-fundacion-page/register-fundacion-page.component';
+import { MenuComponent } from './shared/menu/menu.component';
 import { Page404Component } from './shared/page404/page404.component';
 
 const routes: Routes = [
@@ -31,8 +33,16 @@ const routes: Routes = [
     component: PagosComponent
   },
   {
+    path: 'paypal',
+    component: PaypalComponent
+  },
+  {
     path: 'Administrador',
     component: Page404Component
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
   },
   // {
   //   path: '**',
