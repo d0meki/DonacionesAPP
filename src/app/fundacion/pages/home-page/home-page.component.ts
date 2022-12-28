3b0e69d5f898c5f5069ada0ddd9f5d410d8772de
JhonatanWCL99 <wilsoncoyoleon@gmail.com>
2022-12-27 22:10:06 -0400

    zz

--- src/app/fundacion/pages/home-page/home-page.component.ts
+++ src/app/fundacion/pages/home-page/home-page.component.ts
@@ -2,30 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { Proyecto } from '../../interfaces/projects-interface';
 import { FundacionService } from '../../services/fundacion.service';
-
+import { WalletService } from '../../../shared/services/wallet.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  public foundation_id : number = 0;
-  public Projects : Proyecto[] = [];
-  public spinnerState : boolean = true;
-  constructor(private authService : AuthService, private fundacionService : FundacionService) { }
+  public foundation_id: number = 0;
+  public Projects: Proyecto[] = [];
+  public spinnerState: boolean = true;
+  constructor(private authService: AuthService, private fundacionService: FundacionService, private walletService: WalletService) { }
 
   ngOnInit(): void {
     const user = this.authService.user;
     this.foundation_id = user.person.foundation.id;
     this.fundacionService.getProjects(this.foundation_id)
-    .subscribe( (res: Proyecto[]) => {
-      this.Projects = res;
-      // console.log(res)
-      this.spinnerState = false
-    });
+      .subscribe((res: Proyecto[]) => {
+        this.Projects = res;
+        // console.log(res)
+        this.spinnerState = false
+      });
+
+    this.walletService.connectWallet().then(res => {
+      console.log(res)
+    })
   }
 
-  get projectsList(){
+  get projectsList() {
     return this.Projects;
   }
 
--- src/app/fundacion/pages/project-page/project-page.component.ts
+++ src/app/fundacion/pages/project-page/project-page.component.ts
@@ -10,13 +10,13 @@ import { FundacionService } from '../../services/fundacion.service';
   styleUrls: ['./project-page.component.css']
 })
 export class ProjectPageComponent implements OnInit {
-  private files : any[] = [];
-  private foundation_id : number = 0;
+  private files: any[] = [];
+  private foundation_id: number = 0;
 
-  public customForm : FormGroup = this.formBuilder.group({
-    'name': ['', [Validators.required, ]],
-    'reason': ['', [Validators.required, ]],
-    'description': [ '',Validators.required],
+  public customForm: FormGroup = this.formBuilder.group({
+    'name': ['', [Validators.required,]],
+    'reason': ['', [Validators.required,]],
+    'description': ['', Validators.required],
     'requested_amount': [0, [Validators.required]],
     'project_type': ['', [Validators.required]],
     'bank': ['', Validators.required],
@@ -26,54 +26,55 @@ export class ProjectPageComponent implements OnInit {
     "publication_date": ["2022-08-12", [Validators.required]],
     'state': [false]
   });
-  constructor(private formBuilder : FormBuilder, private fundacionService : FundacionService, private authService : AuthService, private router : Router) { }
+  constructor(private formBuilder: FormBuilder, private fundacionService: FundacionService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     const user = this.authService.user;
     this.foundation_id = user.person.foundation.id;
-    this.customForm.patchValue({foundation_id: this.foundation_id});
+    this.customForm.patchValue({ foundation_id: this.foundation_id });
+   
   }
-  fieldValidator(field: string){
+  fieldValidator(field: string) {
     return this.customForm.controls[`${field}`].errors && this.customForm.controls[`${field}`].touched;
   }
-  submitProject(){
-    if(this.files.length > 1){
-      this.customForm.patchValue({ photos : ["1", "2"]})
+  submitProject() {
+    if (this.files.length > 1) {
+      this.customForm.patchValue({ photos: ["1", "2"] })
     }
-    if(this.customForm.invalid){
+    if (this.customForm.invalid) {
       this.customForm.markAllAsTouched();
     } else {
-      let array : string[] = [];
-      this.files.forEach( (image: any, index: number) => {
+      let array: string[] = [];
+      this.files.forEach((image: any, index: number) => {
         this.uploadImage(index, array);
       });
       setTimeout(() => {
         this.fundacionService.createProject(this.customForm.value)
-        .subscribe( res => {
-          console.log(res);
-          console.log(this.customForm.value)
-          this.router.navigate(['fundacion/home'])
-        })
+          .subscribe(res => {
+            console.log(res);
+            console.log(this.customForm.value)
+            this.router.navigate(['fundacion/home'])
+          })
       }, 2000);
     }
   }
-  addImage(image: any, number : number){
+  addImage(image: any, number: number) {
     this.files[number] = image;
     console.log(this.files);
   }
 
-  uploadImage(index : number, array : string[]){
+  uploadImage(index: number, array: string[]) {
     const fileImages = new FormData();
-      fileImages.append('file', this.files[index]);
-      fileImages.append('upload_preset', 'donaciones_cloudinary');
-      fileImages.append('cloud_name', this.fundacionService.cloudinaryKey)
+    fileImages.append('file', this.files[index]);
+    fileImages.append('upload_preset', 'donaciones_cloudinary');
+    fileImages.append('cloud_name', this.fundacionService.cloudinaryKey)
     this.fundacionService.uploadImage(fileImages)
-    .subscribe( (res) => {
-      array.push(res.url);
-      this.customForm.patchValue({photos: array})
-    })
+      .subscribe((res) => {
+        array.push(res.url);
+        this.customForm.patchValue({ photos: array })
+      })
   }
-  changeProjectType(event: any){
-    this.customForm.patchValue({project_type : event.target.value})
+  changeProjectType(event: any) {
+    this.customForm.patchValue({ project_type: event.target.value })
   }
 }
